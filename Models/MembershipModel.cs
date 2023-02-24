7e568861846ff1c6e5393d57690024f82f97c556
Iordache Catalin <iordache.catalin1986@gmail.com>
2023-02-24 15:36:26 +0200

    Adding Models

--- Models/MembershipModel.cs
+++ Models/MembershipModel.cs
@@ -1,6 +1,23 @@
-﻿namespace MyFirstMVCAppIordacheCatalin.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstMVCAppIordacheCatalin.Models
 {
     public class MembershipModel
     {
+
+        [Key]
+        public Guid? IDMembership { get; set; }
+
+        public Guid? IDMember { get; set; }
+
+        public Guid? IDMembershipType { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int Level { get; set; }
+
+
     }
 }
